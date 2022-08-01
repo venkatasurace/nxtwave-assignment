35fdb2fab315dcdf493933d616bbbe2bec283e06
venkatasurace <106607525+venkatasurace@users.noreply.github.com>
2022-08-01 17:15:15 +0530

    craped data

--- project/crawl_quotes.py
+++ project/crawl_quotes.py
@@ -3,6 +3,8 @@ import requests
  
 from bs4 import BeautifulSoup
 
+import pandas as pd
+
 url = "http://quotes.toscrape.com/"
 
 response = requests.get(url)
@@ -12,20 +14,30 @@ soup = BeautifulSoup(response.content,"html.parser")
 
 quotes = soup.find_all("span" ,class_ = "text")
 
-quotes = [quote.text[1:-1] for quote in quotes]
+quotes = [quote.text[1:-1] for quote in quotes] # list of quotes
 
 authors = soup.find_all("small" ,class_ = "author")
 
-authors = [author.text[1:-1] for author in authors]
+authors = [author.text[1:-1] for author in authors] #list of authors
 
 tags = soup.find_all("div" ,class_ = "tags")
 
-total_tags = []
+total_tags = [] #list of tags
 
 for i in range(len(tags)):
     k = []
-    for j in tags[i].find_all("a",class_= "tag"):
+    for j in tags[i].find_all("a", class_= "tag"):
         k.append(j.text)
     total_tags.append("," .join(k))
-print(total_tags)
 
+
+
+dataset = pd.DataFrame()
+
+dataset["Quote"] = quotes
+dataset["author"] = authors
+dataset["Tags"] = total_tags
+
+print(dataset)
+
+dataset.to_csv("quotes.css")
