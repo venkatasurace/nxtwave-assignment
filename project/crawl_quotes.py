b3109bcc9fe82c5444488f6a1b8897dcde290290
venkatasurace <106607525+venkatasurace@users.noreply.github.com>
2022-08-07 12:19:12 +0530

    usepprint

--- project/crawl_quotes.py
+++ project/crawl_quotes.py
@@ -1,7 +1,8 @@
-
 import requests
  
 from bs4 import BeautifulSoup
+
+import pprint
 
 import pandas as pd
 
@@ -13,20 +14,19 @@ soup = BeautifulSoup(response.content,"html.parser")
 
 tittles = soup.select('.text')
 authors = soup.select('.author')
-tags = soup.select('.keywords')
-
-
+tags = soup.select(".keywords")
 
 
 def data_crawl_quotes(tittles,authors,tags):
-    data_list = []
-    for quote,author,tag in tittles,authors,tags:
+    list_a = []
+    for quote,author,tag in zip(tittles,authors,tags):
         quote_text = quote.getText()
         author_text = author.getText()
-        tags_text = tag.gettext()
-        print(author_text)
+        tags_text = tag.get("content")
+        list_a.append({ "quotes":quote_text,"author":author_text,"Tags":tags_text})
+    return list_a
 
-data_crawl_quotes(tittles,authors,tags)
+pprint.pprint(data_crawl_quotes(tittles,authors,tags))
 
 
 
