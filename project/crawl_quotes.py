e162374e2ae1ee266d0bb474d0c436100258766c
venkatasurace <106607525+venkatasurace@users.noreply.github.com>
2022-08-07 12:55:10 +0530

    all pages data

--- project/crawl_quotes.py
+++ project/crawl_quotes.py
@@ -4,28 +4,36 @@ from bs4 import BeautifulSoup
 
 import pprint
 
-
-url = "http://quotes.toscrape.com/"
-
-response = requests.get(url)
-
-soup = BeautifulSoup(response.content,"html.parser")
-
-tittles = soup.select('.text')
-authors = soup.select('.author')
-tags = soup.select(".keywords")
-
-
 def data_crawl_quotes(tittles,authors,tags):
-    list_a = []
-    for quote,author,tag in zip(tittles,authors,tags):
-        quote_text = quote.getText()
-        author_text = author.getText()
-        tags_text = tag.get("content")
-        list_a.append({ "quotes":quote_text,"author":author_text,"Tags":tags_text})
-    return list_a
+        list_a = []
+        for quote,author,tag in zip(tittles,authors,tags):
+            quote_text = quote.getText()
+            author_text = author.getText()
+            tags_text = tag.get("content")
+            list_a.append({ "quotes":quote_text,"author":author_text,"Tags":tags_text})
+        return list_a
 
-pprint.pprint(data_crawl_quotes(tittles,authors,tags))
+source_url = "http://quotes.toscrape.com/"
+full_list = []
+for i in range(1,11):
+    
+    url = source_url
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content,"html.parser")
+
+    tittles = soup.select('.text')
+    authors = soup.select('.author')
+    tags = soup.select(".keywords")
+
+    complete_data = data_crawl_quotes(tittles,authors,tags)
+    full_list.append(complete_data)
+    url = "http://quotes.toscrape.com/page/"+"str(i)"+"/"
+
+
+
+
+pprint.pprint(full_list)
 
 
 
